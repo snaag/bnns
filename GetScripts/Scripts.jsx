2a07b4243ad911e826df57e79fba1e1f0b0e972e
Yuni-Q <carnotcycle2@naver.com>
2019-06-11 18:27:52 +0900

    feat: get element

--- GetScripts/Scripts.jsx
+++ GetScripts/Scripts.jsx
@@ -8,14 +8,12 @@ const Scripts = () => {
   const [episodes, setEpisodes] = useState(0);
   const mounted = useRef(false);
   const cors_url = 'https://cors-anywhere.herokuapp.com/';
-  const base_url = 'https://www.springfieldspringfield.co.uk/episode_scripts.php?tv-show=brooklyn-nine-nine';
+  const base_url =
+    'https://www.springfieldspringfield.co.uk/episode_scripts.php?tv-show=brooklyn-nine-nine';
 
-
-  const onChangeContents = (e) => {
-    if(e.target.name === 'seasons')
-      setSeasons(e.target.value);
-    else
-      setEpisodes(e.target.value);
+  const onChangeContents = e => {
+    if (e.target.name === 'seasons') setSeasons(e.target.value);
+    else setEpisodes(e.target.value);
   };
 
   const onClickDownload = () => {
@@ -30,21 +28,22 @@ const Scripts = () => {
   // Brooklyn nine-nine의 모든 season 목록을 가져오는 함수
   const getSeasonsList = async () => {
     try {
-      const html = await axios.get(cors_url+base_url,{headers: {'Access-Control-Allow-Origin': '*'}});
+      const html = await axios.get(cors_url + base_url, {
+        headers: { 'Access-Control-Allow-Origin': '*' },
+      });
 
       // const str = html.data.replace(/<br> /g, '\n');
 
-
       // dowajooseyoooooo
       const document = html.data;
-      console.log(document.querySelector('.season-episodes').length);
+      const str = document.replace(/<br> /g, '\n');
+      let $ = cheerio.load(str);
+      console.log($('.season-episodes').length);
 
-      // let $ = cheerio.load(str);
       // const seasons = $('#content_container > div.main-content > div.main-content-left > div:nth-child(7)').text().trim();
       // document를 얻어와서 document.querySelectorAll('.season-episodes').length;를 쓰고싶은데 어떡하지
 
       // console.log(seasons);
-
     } catch (error) {
       console.log(error);
     }
@@ -53,12 +52,19 @@ const Scripts = () => {
   // 임의의 script, title을 가져오는 함수
   const getScripts = async () => {
     try {
-      const html = await axios.get(`https://cors-anywhere.herokuapp.com/https://www.springfieldspringfield.co.uk/view_episode_scripts.php?tv-show=brooklyn-nine-nine&episode=s01e01`,{headers: {'Access-Control-Allow-Origin': '*'}});
+      const html = await axios.get(
+        `https://cors-anywhere.herokuapp.com/https://www.springfieldspringfield.co.uk/view_episode_scripts.php?tv-show=brooklyn-nine-nine&episode=s01e01`,
+        { headers: { 'Access-Control-Allow-Origin': '*' } },
+      );
 
       const str = html.data.replace(/<br> /g, '\n');
       let $ = cheerio.load(str);
-      const title = $('h3').text().trim();
-      const scripts = $('div.scrolling-script-container').text().trim();
+      const title = $('h3')
+        .text()
+        .trim();
+      const scripts = $('div.scrolling-script-container')
+        .text()
+        .trim();
       console.log(title);
       console.log(scripts);
     } catch (error) {
@@ -66,30 +72,28 @@ const Scripts = () => {
     }
   };
 
-
   useEffect(() => {
     // componentDidMount
-    if(!mounted.current) {
+    if (!mounted.current) {
       mounted.current = true;
-
     } else {
       window.alert(`${seasons} and ${episodes}`);
     }
-  },[episodes, seasons]);
-
-
+  }, [episodes, seasons]);
   return (
     <>
-      <h3>Seasons: {seasons}, Episodes: {episodes}</h3>
+      <h3>
+        Seasons: {seasons}, Episodes: {episodes}
+      </h3>
 
       <div>
         <h3>Seasons</h3>
-        <input type='number' name='seasons' value={seasons} onChange={onChangeContents} />
+        <input type="number" name="seasons" value={seasons} onChange={onChangeContents} />
         <button onClick={onClickSeason}>Next</button>
       </div>
 
       <h3>Episodes</h3>
-      <input type='number' name='episodes' value={episodes} onChange={onChangeContents} />
+      <input type="number" name="episodes" value={episodes} onChange={onChangeContents} />
       <button onClick={onClickDownload}>Download</button>
     </>
   );
