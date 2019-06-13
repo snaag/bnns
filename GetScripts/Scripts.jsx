39fdf545eeaa15d4ee8141f85c0eb5fe2e2a355b
Yuni-Q <carnotcycle2@naver.com>
2019-06-13 16:20:43 +0900

    feat: add styled-components

--- GetScripts/Scripts.jsx
+++ GetScripts/Scripts.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useRef, useEffect } from 'react';
 const cheerio = require('cheerio');
 const axios = require('axios');
-import style from './Script.module.css';
+import styled from 'styled-components';
+
+const StyledH1 = styled.h1`
+  margin: 40px;
+  border: 5px dotted blue;
+`;
 
 const Scripts = () => {
   const [seasons, setSeasons] = useState([]);
@@ -21,7 +26,6 @@ const Scripts = () => {
   const scriptUrl =
     'https://www.springfieldspringfield.co.uk/view_episode_scripts.php?tv-show=brooklyn-nine-nine&episode=';
 
-
   const getSeasons = async () => {
     try {
       const html = await axios.get(cors_url + bnn_url, {
@@ -34,32 +38,32 @@ const Scripts = () => {
       const cnt_seasons = $('.season-episodes').length;
       const newSeasons = [];
 
-      for (let a=1; a < cnt_seasons+1; a++) {
-        const name_season = $(`#season${a}`).text().trim();
+      for (let a = 1; a < cnt_seasons + 1; a++) {
+        const name_season = $(`#season${a}`)
+          .text()
+          .trim();
         const e = $(`#season${a} ~ a`);
         newSeasons.push(name_season);
 
         const cntEpisode = e.length;
         const eachEpisodes = [];
-        for (let b=0; b < cntEpisode; b++) {
+        for (let b = 0; b < cntEpisode; b++) {
           eachEpisodes.push(e[b].children[0].data);
         }
         episodes.current.push(eachEpisodes);
       }
-      setSeasons((prevSeasons) => [...prevSeasons, ...newSeasons]);
-
+      setSeasons(prevSeasons => [...prevSeasons, ...newSeasons]);
     } catch (error) {
       console.error(error);
     }
-
   };
 
-  const clickSeason = (e) => {
-    console.log("clickSeason");
+  const clickSeason = e => {
+    console.log('clickSeason');
     click.current = true;
     const idx = e.target.name - 1;
 
-    setSeasonIdx(idx+1);
+    setSeasonIdx(idx + 1);
 
     setEpisodeNow(episodes.current[idx]);
     setEpisodeIdx(-1);
@@ -69,24 +73,22 @@ const Scripts = () => {
     setScript('');
   };
 
-  const clickEpisode = (e) => {
+  const clickEpisode = e => {
     setEpisodeIdx(e.target.name);
   };
 
   // 임의의 script, title을 가져오는 함수
   const getScripts = async () => {
-
     setTitle('');
-    let info = "s";
-    seasonIdx > 9 ? info+=seasonIdx : info+="0"+seasonIdx ;
-    info = info + "e";
-    episodeIdx > 9 ? info+=episodeIdx : info+="0"+episodeIdx ;
+    let info = 's';
+    seasonIdx > 9 ? (info += seasonIdx) : (info += '0' + seasonIdx);
+    info = info + 'e';
+    episodeIdx > 9 ? (info += episodeIdx) : (info += '0' + episodeIdx);
 
     try {
-      const html = await axios.get(
-        cors_url+scriptUrl+info,
-        { headers: { 'Access-Control-Allow-Origin': '*' } },
-      );
+      const html = await axios.get(cors_url + scriptUrl + info, {
+        headers: { 'Access-Control-Allow-Origin': '*' },
+      });
 
       const str = html.data.replace(/<br> /g, '\n');
       let $ = cheerio.load(str);
@@ -104,68 +106,77 @@ const Scripts = () => {
     }
   };
 
-  const clickDownload = () => {
-
-  };
+  const clickDownload = () => {};
 
   useEffect(() => {
-    if(episodeIdx > 0) {
+    if (episodeIdx > 0) {
       getScripts();
     }
-  },[episodeIdx]);
+  }, [episodeIdx]);
 
   useEffect(() => {
     scriptClicked.current = true;
-  },[seasonIdx]);
-
+  }, [seasonIdx]);
 
   useEffect(() => {
-
-    if (!mounted.current) { // componentDidMount
+    if (!mounted.current) {
+      // componentDidMount
       mounted.current = true;
       getSeasons();
-    } else { // componentDidUpdate
+    } else {
+      // componentDidUpdate
     }
   }, [seasons]);
   return (
     <>
-      {episodes.current.length<1 ?
-        <h1 className={style.loadingPage}>Loading...</h1>:
+      {episodes.current.length < 1 ? (
+        <StyledH1>Loading...</StyledH1>
+      ) : (
         <div>
           {/*{episodeIdx > 0 && <p className="info">You choose the {seasonIdx}-{episodeIdx}</p>}*/}
           <h2>Seasons</h2>
           <h3>
-            {seasons.map((v) => {
+            {seasons.map(v => {
               return (
-                <button className="season" name={/\d+/.exec(v)} onClick={clickSeason}>{v}</button>
-              )
+                <button className="season" name={/\d+/.exec(v)} onClick={clickSeason}>
+                  {v}
+                </button>
+              );
             })}
           </h3>
         </div>
-      }
+      )}
 
-      {click.current &&
+      {click.current && (
         <div>
           <h2>Episodes</h2>
-          {episodeNow.map((v) => {
+          {episodeNow.map(v => {
             return (
-              <li><button className="episode" name={/^\d+/.exec(v)} onClick={clickEpisode}>{v}</button></li>
-            )
+              <li>
+                <button className="episode" name={/^\d+/.exec(v)} onClick={clickEpisode}>
+                  {v}
+                </button>
+              </li>
+            );
           })}
         </div>
-      }
+      )}
 
-      {
-        (scriptClicked.current && click.current) &&
-          (title.length < 2 ?
-              <h4 className="loadingScript">S{seasonIdx}-E{episodeIdx} script is loading...</h4> :
-              <div className="scriptPart">
-                <pre className="title">{title}</pre>
-                <button className="download" onClick={clickDownload}>download</button>
-                <pre className="script">{script}</pre>
-              </div>
-          )
-      }
+      {scriptClicked.current &&
+        click.current &&
+        (title.length < 2 ? (
+          <h4 className="loadingScript">
+            S{seasonIdx}-E{episodeIdx} script is loading...
+          </h4>
+        ) : (
+          <div className="scriptPart">
+            <pre className="title">{title}</pre>
+            <button className="download" onClick={clickDownload}>
+              download
+            </button>
+            <pre className="script">{script}</pre>
+          </div>
+        ))}
     </>
   );
 };
--- GetScripts/webpack.config.js
+++ GetScripts/webpack.config.js
@@ -1,41 +1,42 @@
 const path = require('path');
 
 module.exports = {
-    name: 'getscripts',
-    mode: 'development',
-    devtool: 'eval',
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    entry: {
-        app: './client',
-    },
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            loader: 'babel-loader',
-            options: {
-                presets: [
-                    '@babel/preset-react',
-                ],
-                plugins: [
-                    '@babel/plugin-proposal-class-properties',
-                    'react-hot-loader/babel'
-                ],
-            },
-            exclude: path.join(__dirname, 'node_modules'),
-        }],
-    },
-    plugins: [],
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].js',
-        publicPath: '/dist',
-    },
-    node: {
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty',
-        dns: 'empty'
-    }
+  name: 'getscripts',
+  mode: 'development',
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  entry: {
+    app: './client',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react'],
+          plugins: [
+            '@babel/plugin-proposal-class-properties',
+            'react-hot-loader/babel',
+            'babel-plugin-styled-components',
+          ],
+        },
+        exclude: path.join(__dirname, 'node_modules'),
+      },
+    ],
+  },
+  plugins: [],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/dist',
+  },
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty',
+  },
 };
